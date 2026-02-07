06336fcfaa4968f2f97f85ce9e8f975d3f95ed40
sanjanarai25082005-afk <sanjanarai25082005@gmail.com>
2026-02-07 15:37:05 +0530

    added createquotation

--- BillingShop/Frontend/src/App.tsx
+++ BillingShop/Frontend/src/App.tsx
@@ -4,6 +4,9 @@ import Sidebar from "./components/Cashier/Sidebar";
 import Navbar from "./components/Cashier/Navbar";
 import CashierDashboard from "./components/CashierDashboard/CashierDashboard";
 import Createparty from "./components/Cashier/Createparty";
+import CreateQuotation from "./components/Cashier/CreateQuotation";
+import ProformaInvoice from "./components/Cashier/ProformaInvoice";
+import SalesReturn from "./components/Cashier/SalesReturn";
  
 /* Dummy pages (replace later with real pages) */
 const Page = ({ title }: { title: string }) => (
@@ -43,12 +46,12 @@ function App() {
           <Route path="/create-party" element={<Createparty />} />
           <Route path="/create-item" element={<Page title="Create Item" />} />
 
-          <Route path="/quotation" element={<Page title="Quotation" />} />
+          <Route path="/quotation" element={<CreateQuotation />} />
           <Route path="/payment-in" element={<Page title="Payment In" />} />
-          <Route path="/sales-return" element={<Page title="Sales Return" />} />
+          <Route path="/sales-return" element={<SalesReturn />} />
           <Route path="/credit-note" element={<Page title="Credit Note" />} />
           <Route path="/delivery-challan" element={<Page title="Delivery Challan" />} />
-          <Route path="/proforma-invoice" element={<Page title="Proforma Invoice" />} />
+          <Route path="/proforma-invoice" element={<ProformaInvoice />} />
 
           <Route path="/purchase" element={<Page title="Purchase" />} />
           <Route path="/payment-out" element={<Page title="Payment Out" />} />
