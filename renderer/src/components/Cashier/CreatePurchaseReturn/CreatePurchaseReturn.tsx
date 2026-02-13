f51cc2f8aed4cecc16a5e931d97ae699f54e5c1e
rohit1604jha@gmail.com <rohit2004jha@gmail.com>
2026-02-13 07:30:57 +0530

    setting ui

--- renderer/src/components/Cashier/CreatePurchaseReturn/CreatePurchaseReturn.tsx
+++ renderer/src/components/Cashier/CreatePurchaseReturn/CreatePurchaseReturn.tsx
@@ -1,26 +1,37 @@
+import { useState } from "react";
 import Navbar from "../Navbar";
 import BillForm from "../Billform";
+import QuickVoucherSettingsModal from "../QuickQuotationSettingsModal/QuickVoucherSettingsModal";
 
 export default function CreatePurchaseReturn() {
+  const [openSettings, setOpenSettings] = useState(false);
+
   return (
     <>
       <Navbar
-  title="Create Purchase Return"
-  showBackButton={true}
-  backPath="/dashboard"
-  showSettings={true}
-  settingsLabel="Settings"
-  primaryAction={{
-    label: "Save",
-    onClick: () => console.log("Save clicked"),
-  }}
-  secondaryAction={{
-    label: "Save & New",
-    onClick: () => console.log("Save & New clicked"),
-  }}
-/>
+        title="Create Purchase Return"
+        showBackButton
+        backPath="/dashboard"
+        showSettings
+        onSettingsClick={() => setOpenSettings(true)}   
+        primaryAction={{
+          label: "Save",
+          onClick: () => console.log("Save clicked"),
+        }}
+        secondaryAction={{
+          label: "Save & New",
+          onClick: () => console.log("Save & New clicked"),
+        }}
+      />
 
       <BillForm mode="purchaseReturn" />
+
+      {openSettings && (
+        <QuickVoucherSettingsModal
+          type="purchaseReturn"   
+          onClose={() => setOpenSettings(false)}
+        />
+      )}
     </>
   );
 }
--- renderer/src/components/Cashier/DeliveryChallan/DeliveryChallan.tsx
+++ renderer/src/components/Cashier/DeliveryChallan/DeliveryChallan.tsx
@@ -1,31 +1,41 @@
+import { useState } from "react";
 import Navbar from "../Navbar";
 import BillForm from "../Billform";
+import QuickVoucherSettingsModal from "../QuickQuotationSettingsModal/QuickVoucherSettingsModal";
 
-export default function DeliveryChallan(){
-    return(
-        <>
-        <Navbar
+export default function DeliveryChallan() {
+  const [openSettings, setOpenSettings] = useState(false);
+
+  return (
+    <>
+      <Navbar
         title="Create Delivery Challan"
-        showBackButton={true}
+        showBackButton
         backPath="/dashboard"
-
-        showSettings={true}
-
+        showSettings
+        onSettingsClick={() => setOpenSettings(true)}   // 🔥 IMPORTANT
         primaryAction={{
           label: "Save",
           onClick: () => {
-            console.log("Save Debit Note");
+            console.log("Save Delivery Challan");
           },
         }}
-
         secondaryAction={{
           label: "Save & New",
           onClick: () => {
-            console.log("Save & New Debit Note");
+            console.log("Save & New Delivery Challan");
           },
         }}
       />
+
       <BillForm mode="deliveryChallan" />
-        </>
-    );
+
+      {openSettings && (
+        <QuickVoucherSettingsModal
+          type="deliveryChallan"   // 🔥 IMPORTANT
+          onClose={() => setOpenSettings(false)}
+        />
+      )}
+    </>
+  );
 }
